677ea07a34220a10e508ffc7b2f5fcfd89d66b78
Aniket Mohanty <aniketmohanty@google.com>
2026-05-20 19:26:23 +0000

    updated and added new example scripts and new runtime_env for sglang integration

--- integration/verl/examples/run_qwen2_5-32b_math_sglang.sh
+++ integration/verl/examples/run_qwen2_5-32b_math_sglang.sh
@@ -45,7 +45,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
-    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.name=sglang \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
     actor_rollout_ref.rollout.checkpoint_engine.update_weights_bucket_megabytes=4096 \
     actor_rollout_ref.rollout.n=5 \
